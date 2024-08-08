0e4ebd254cbb97f488a8c0727dd4902c9ff44791
Nokiller103 <indikidanime@gmail.com>
2024-08-08 15:55:39 +0000

    add mustache, json and other style package

---
+++ src/jobs.js
@@ -0,0 +1,35 @@
+const JOBS = [
+{
+    id: 1,
+    title: 'frontend developer',
+    location: 'Bengaluru, India',
+    salary: '12,00,000',
+    posted: 'March 3,2024'
+},
+
+{
+    id: 2,
+    title: 'fullstack developer',
+    location: 'New Delhi,India',
+    salary: '15,00,000',
+    posted: 'Feb 1, 2024'
+},
+
+{
+    id: 3,
+    title: 'data-scientist',
+    location: 'San Francisco,USA',
+    salary: '$175,000',
+    posted: 'Dec 22, 2024'
+},
+
+{
+    id: 4,
+    title: 'data-scientist',
+    location: 'Remote',
+    salary: '$80,000',
+    posted: 'Sep 19, 2024'
+},
+
+]
+module.exports = JOBS; 
